13434f5e28d124432c6842adb9b8e6e9bfb4eea0
Lexline Johnson <lexlinejj27@gmail.com>
2025-01-25 14:27:16 -0500

    Save mood

--- Better Gator/HomeView.swift
+++ Better Gator/HomeView.swift
@@ -19,70 +19,73 @@ struct HomeView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @State var currentEmotion: Emotion? = nil
     var body: some View {
-        VStack {
-            Text("How are you feeling?")
-                .font(.title)
-            HStack {
-                ZStack {
-                    Rectangle()
-                        .fill(Color(UIColor.secondarySystemBackground))
-                        .frame(height: 100)
-                        .cornerRadius(10)
+        NavigationView {
+            VStack {
+                Text("How are you feeling?")
+                    .font(.headline)
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .fill(Color(UIColor.secondarySystemBackground))
+                            .frame(height: 100)
+                            .cornerRadius(10)
+                            .padding()
+                        HStack {
+                            Image("excited")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 200)
+                                .onTapGesture {
+                                    currentEmotion = .angry
+                                }
+                            Image("happy")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 150)
+                                .onTapGesture {
+                                    currentEmotion = .sad
+                                }
+                            Image("calm")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 150)
+                                .onTapGesture {
+                                    currentEmotion = .calm
+                                }
+                            Image("sad")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 150)
+                                .onTapGesture {
+                                    currentEmotion = .happy
+                                }
+                            Image("angry")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(height: 150)
+                                .onTapGesture {
+                                    currentEmotion = .excited
+                                }
+                        }
                         .padding()
-                    HStack {
-                        Image("excited")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 200)
-                            .onTapGesture {
-                                currentEmotion = .angry
-                            }
-                        Image("happy")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 150)
-                            .onTapGesture {
-                                currentEmotion = .sad
-                            }
-                        Image("calm")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 150)
-                            .onTapGesture {
-                                currentEmotion = .calm
-                            }
-                        Image("sad")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 150)
-                            .onTapGesture {
-                                currentEmotion = .happy
-                            }
-                        Image("angry")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 150)
-                            .onTapGesture {
-                                currentEmotion = .excited
-                            }
                     }
-                    .padding()
                 }
-            }
-            Button(action: {
-                guard let currentEmotion = currentEmotion else { return }
-                let newMood = Mood(context: viewContext)
-                newMood.emotion = Int32(currentEmotion.rawValue)
-                newMood.timestamp = Date()
-                do {
-                    try viewContext.save()
-                } catch {
-                    print("error!!")
+                Button(action: {
+                    guard let currentEmotion = currentEmotion else { return }
+                    let newMood = Mood(context: viewContext)
+                    newMood.emotion = Int32(currentEmotion.rawValue)
+                    newMood.timestamp = Date()
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        print("error!!")
+                    }
+                }) {
+                    Text("Log emotion")
                 }
-            }) {
-                Text("Log emotion")
+                .buttonStyle(.borderedProminent)
             }
-            .buttonStyle(.borderedProminent)
+            .navigationTitle("Hello, Name")
         }
     }
 }
